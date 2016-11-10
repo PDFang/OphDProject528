e3a315fd980da25e4e1e39ba9563ef878a0e7395
Jeff Arrowsmith <jeff.arrowsmith@incontact.com>
2016-11-10 10:08:37 -0700

    Remove unnecessary comments from Community Header Component

--- src/aura/CommunityNavBar/CommunityNavBarController.js
+++ src/aura/CommunityNavBar/CommunityNavBarController.js
@@ -1,10 +1,6 @@
 ({
-	doInit : function(component, event, helper) {
-        var toggle = false;
-        var links = [{name: 'PartnerCommunity', label: 'Partner Community', url: 'https://www.salesforce.incontact.com', isVisible: true },
-                     {name: 'CustomerCommunity', label: 'Customer Community', url: 'http://www.microsoft.com', isVisible: toggle },
-                     {name: 'Support Site', label: 'Support Site', url: 'https://www.support.incontact.com', isVisible: true }];
 
+	doInit : function(component, event, helper) {
         var action = component.get('c.getPermissionSets');
         action.setCallback(this, function(response) {
            var state = response.getState();
@@ -13,7 +9,6 @@
             }
         });
         $A.enqueueAction(action);
-        /*component.set("v.CommunityLinks", links);*/
 	},
 
 })
