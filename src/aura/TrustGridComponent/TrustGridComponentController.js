c0d91825c9163d13896c693b7b45147a6b29969c
mohandaas.rangaswamy <mohandaas.rangaswamy@incontact.com>
2016-11-14 16:09:12 -0700

    Task #23974 - 83031 - Salesforce Lightning - Trust Grid - Maintenance Calendar Transition - Create an Apex Controller to get the Events and Platforms

--- src/aura/TrustGridComponent/TrustGridComponentController.js
+++ src/aura/TrustGridComponent/TrustGridComponentController.js
@@ -5,13 +5,18 @@
 
     loadDates: function(component, event, helper) {
         console.log("scripts loaded");
-         helper.getPastSevenDates(component);
+         //helper.getPastSevenDates(component);
+
     },
 
     loadPrevious : function(component, event, helper){
          helper.getFutureDates(component);
     },
 
+    doIntit:function(component, event, helper) {
+      console.log("call do init");
+      helper.loadTableData(component);
+    },
     doneRendering: function(component){
        /** jQuery.noConflict();
         var firstDate = '0_date';
--- src/aura/TrustGridComponent/TrustGridComponentHelper.js
+++ src/aura/TrustGridComponent/TrustGridComponentHelper.js
@@ -8,6 +8,7 @@
         var dates  = [];
         var lastDate  = component.get("v.lastDate");
         var d = lastDate == "" ? new Date() : lastDate;
+        component.set("v.firstDate", d);
         var pastDt;
         var today = new Date();
         var priorDate =  new Date(today.getFullYear(), today.getMonth(), today.getDate() - 30);
@@ -40,15 +41,17 @@
             var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
                       "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
             var dates  = [];
-            var lastDate  = component.get("v.lastDate");
-            var d = new Date(lastDate.getFullYear(), lastDate.getMonth(), lastDate.getDate() + 7);
+            var firstDate  = component.get("v.firstDate");
+            var d = new Date(firstDate);
+            console.log(d);
+             var today = new Date();
             var pastDt;
-            var today = new Date();
-            for(var i = 0; i < 7; i++){
-                pastDt = new Date(d.getFullYear(), d.getMonth(), d.getDate() + i);
+            for(var i = 7; i > 0; i--){
+                pastDt = new Date(d.getFullYear(), d.getMonth(), d.getDate() - i);
                 var monthName = monthNames[pastDt.getMonth()];
                 var date = monthName + ' ' + pastDt.getDate();
                 dates.push(date);
+                console.log(i);
             }
             if(today.getTime() == pastDt.getTime())
             {
@@ -56,7 +59,7 @@
              var obj = jQuery('li.previous');
              obj.addClass("disabled");
             }
-            else if(lastDate != ''){
+            else {
 
                 jQuery.noConflict();
                 var obj = jQuery('li.next');
@@ -64,8 +67,102 @@
                 obj.removeClass("disabled");
             }
             component.set("v.dates", dates);
-            component.set("v.lastDate", pastDt);
+            component.set("v.lastDate", new Date(d.getFullYear(), d.getMonth(), d.getDate() + 7));
 
-        }
+        },
 
+        loadTableData: function(component, helper){
+         console.log('came here');
+         var myAction = component.get('c.getPlatformInfo');
+         // Handle returned results...
+
+         myAction.setCallback(component, function(response) {
+                var state = response.getState();
+
+                if(component.isValid() && state === "SUCCESS") {
+                    var result = response.getReturnValue();
+                    var platforms = [];
+                    var tableData = [];
+
+                   //console.log('result =>' + JSON.stringify(result));
+                    // result = JSON.stringify(result);
+                        console.log('result =>' + result);
+
+                    // Loop through results; remember, this is a list of sObjects
+                    for(var item in result) {
+                        // Get the record
+                         var temp =  result[item];
+                         //temp = JSON.stringify(temp);
+                         console.log('temp =>' + JSON.stringify(temp));
+                         platforms.push(temp.platformName);
+                         tableData.push(temp);
+
+                       }
+
+                       component.set('v.datalist', tableData);
+
+                       console.log('platforms =>' + platforms);
+
+
+
+                      /**  // Pull out the field that's going to be my key
+                       var myKey = myRecord.someField__c
+
+                        // Keep track of whether our key already exists in myObjectMap
+                        var found = false;
+                        // Loop through our map and see if an entry for our key exists
+                        for(var x=0; x<myObjectMap.length; x++) {
+                            // Look to see if our object has a "key" value
+                            // and whether that key is equal to the key we want
+                            // to use to group everything
+                            if("key" in myObjectMap[x] && myObjectMap[x]["key"] == myKey) {
+                                myObjectMap[x]["list"].push(myRecord);
+                                // We found our key and pushed the record into its
+                                // list, no need to continue
+                                found = true;
+                                break;
+                            }
+                        }
+                        // Need to make sure this record found a home; if it didn't
+                        // then we need to initialize it in our "map"
+                        if(!found) {
+                            var temp = { "key": myKey, "list": [myRecord] };
+                            myObjectMap.push(temp);
+                        }
+                    }
+
+                    component.set('v.listOfMyCustomObject', myObjectMap);
+
+                    /*
+                    At the end of all this, myObjectMap should look something like...
+                    [
+                        {
+                            "key": "value 1"
+                            , "list": [
+                                {
+                                    ...json representation of myCustomObj__c...
+                                }
+                                , {
+                                    ....another record...
+                                }
+                            ]
+                        }
+                        , {
+                            "key": "value 2"
+                            , "list": [
+                                {
+                                    ...another record...
+                                }
+                            ]
+                        }
+                    ];
+
+                    */
+                } else {
+                    // error handling
+                }
+
+        });
+         $A.enqueueAction(myAction);
+    }
 })
